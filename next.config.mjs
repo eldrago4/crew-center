cc1b21e38d377798df314bca0a17516b52869cb0
eldrago4 <tred38434@gmail.com>
2025-10-25 13:21:56 +0530

    fixed bug on user page which happened due to #16feeb9. added flight manual open version.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
+    optimizePackageImports: [ "@chakra-ui/react" ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/flying-manual',
+        destination: 'https://zeff005.github.io/Flying-Manual/',
+        permanent: true,
+      },
+    ];
   },
 };
 
