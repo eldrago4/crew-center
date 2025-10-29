261d8f21b1d30bd5da67b03f187851a34fbd69c5
eldrago4 <tred38434@gmail.com>
2025-10-29 18:17:57 +0530

    .

--- proxy.js
+++ proxy.js
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
+import { auth } from "@/auth"
 
 export function proxy(request) {
     const { pathname } = request.nextUrl
 
-    // Redirect /crew/* to /maintenance
-    if (pathname.startsWith('/crew/')) {
+    // Redirect /crew and /crew/* to /maintenance
+    if (pathname === '/crew' || pathname.startsWith('/crew/')) {
         return NextResponse.rewrite(new URL('/maintenance', request.url))
     }
 
