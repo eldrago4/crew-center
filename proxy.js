72e4f8d570108c523b279fb726c94984837addf5
Ved Bapardekar <tred38434@gmail.com>
2025-12-01 10:44:03 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -5,12 +5,12 @@ export function proxy(request) {
     const { pathname } = request.nextUrl
 
     // Redirect /crew and /crew/* to /maintenance
-     if (pathname === '/crew' || pathname.startsWith('/crew/')) {
-         return NextResponse.rewrite(new URL('/maintenance', request.url))
-     }
+    // if (pathname === '/crew' || pathname.startsWith('/crew/')) {
+     //    return NextResponse.rewrite(new URL('/maintenance', request.url))
+  //   }
 
-    return NextResponse.next()
-}
+  //  return NextResponse.next()
+// }
 
 export const config = {
     matcher: '/crew/:path*',
